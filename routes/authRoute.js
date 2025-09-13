58608a33ad06c7bc38cee6cf5ed7213bf272343e
RakeshPegu <rakeshpegu903@gmail.com>
2025-09-13 15:17:29 +0530

    added logout api route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -25,4 +25,13 @@ router.post('/login', async(req, res)=>{
         
     }
 })
+router.post('/logout', async(req, res)=>{
+    try {
+        res.status(200).json({success:true , message:"Logged out successfully"})
+        
+    } catch (error) {
+        res.status(500).json({success:false, message:'Something went wrong'})
+        
+    }
+})
 export default router
